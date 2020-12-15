ee35bf867e968f682a77e235bcff1206bb0f8dbd
Reidak18 <lnik18@mail.ru>
2020-12-16 01:13:30 +0300

    Доделал загрузку еластика

--- bash/script.sh
+++ bash/script.sh
@@ -1,10 +1,10 @@
+sudo su
 yum install -y dnf 4.2.23
 sudo rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY*
 sudo yes | dnf install scl-utils
 echo "INSTALL PYTHON"
-dnf install python3 -y
-
-sudo pip3 install vkstreaming
+sudo yes | dnf install python3
+sudo yes | pip3 install vkstreaming
 
 echo "INSTALL GIT"
 sudo yes | dnf install git
@@ -14,8 +14,8 @@ echo "INSTALL JAVA"
 sudo yes | dnf install java-11-openjdk-devel
 
 echo "INSTALL ELASTICSEARCH"
-sudo rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch
-sudo yes | cp DSBDA_HW3/elasticsearch/elasticsearch.repo /etc/yum.repos.d/elasticsearch.repo
-sudo yes | dnf install elasticsearch
+sudo yes | dnf install wget
+wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.9.3-x86_64.rpm -P elastic
+sudo dnf -y localinstall elastic/elasticsearch-7.9.3-x86_64.rpm
 sudo systemctl enable elasticsearch.service
 sudo systemctl start elasticsearch.service
